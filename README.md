7d89d3a7c164ae8a4a130a7c360c59f358d6d587
TermuxHackz <AnonyminHack5@protonmail.com>
2021-09-22 10:56:06 -0400

    initial commit

--- README.md
+++
@@ -1,5 +0,0 @@
-# termuxhackz.github.io
-Just  another web template project
-
-#### Created by
-AnonyminHack5
--- js/popup.js
+++
@@ -1,7 +0,0 @@
-<script>
-// When the user clicks on <div>, open the popup
-function myFunction() {
-  var popup = document.getElementById("myPopup");
-  popup.classList.toggle("show");
-}
-</script>
