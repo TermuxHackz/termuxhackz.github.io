09e8b80e329b0e80d6baef663db5855afadc7774
TermuxHackz <AnonyminHack5@protonmail.com>
2021-10-10 16:08:35 -0400

    initial commit

---
+++ js/popup.js
@@ -0,0 +1,7 @@
+<script>
+// When the user clicks on <div>, open the popup
+function myFunction() {
+  var popup = document.getElementById("myPopup");
+  popup.classList.toggle("show");
+}
+</script>
