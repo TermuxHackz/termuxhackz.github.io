b505bff6f00338bec64ff5e4657ddbebcfc554fc
TermuxHackz <AnonyminHack5@protonmail.com>
2023-04-15 15:54:25 +0100

    Add existing project files to Git

--- js/review.js
+++ js/review.js
@@ -8,14 +8,16 @@ function submitReview() {
     const message = document.getElementById("message").value;
 
     const subject = `New Review from ${name}`;
-    const body = `Name: ${name}%OD%OAEmail: ${email}%OD%OARating: ${rating}%OD%OAMessage: ${message}`;
+    const body = `Name: ${name}\nEmail: ${email}\nRating: ${rating}\nMessage: ${message}`;
 
     window.location.href = `mailto:anonyminhack5@protonmail.com?subject=${subject}&body=${body}`;
 
     swal("Hurry", "Thank you for your review, you will be redirected to the home page in 3s..", "success");
-    form.reset();
-
     setTimeout(() => {
         window.location.href = "index.html";
     }, 3000);
+    
+    form.reset();
+
+    
 }
