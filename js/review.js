d4030ecd0bd82aa5240fa852b003366552fc397d
TermuxHackz <AnonyminHack5@protonmail.com>
2023-04-16 14:24:55 +0100

    Add existing projects files to git

--- js/review.js
+++ js/review.js
@@ -6,14 +6,42 @@ function submitReview() {
     const email =  document.getElementById("email").value;
     const rating = document.getElementById("rating").value;
     const message = document.getElementById("message").value;
+    
 
-    const subject = `New Review from ${name}`;
-    const body = `Name: ${name}\nEmail: ${email}\nRating: ${rating}\nMessage: ${message}`;
+    if (name == "") {
+        swal("Sorry", "Cannot submit this form without filling in your name!!", "error")
+        //alert("Please fill in your name!!");
+        return false
+    }
+    else if  (email == "") {
+        swal("Oops", "Your email address is required for this form to be submitted, kindly fill it and try again", "error")
+        //alert("Please fill in your email!!, Cannot leave it blank bruhhh!");
+        return false
+    }
+    else if (rating == "" ) {
+        swal("Sorry", "Kindly select a rating!! to help us improve!", "error")
+        return false
+    
+    }
+    else if (message == "") {
+        swal("Not again!", "Cannot leave message area blank, please send us yout thoughts, Thank you!", "error")
+        //alert("Cannot leave message field blank, Kindly input something!!");
+        return false
+    }
+    else {
+        const subject = `New Review from ${name}`;
+        const body = `Name: ${name}\n\n Email: ${email}\n\n Rating: ${rating}\n\n Message: ${message}`;
+        window.location.href = `mailto:anonyminhack5@protonmail.com?subject=${subject}&body=${body}`;
+        
+        setTimeout(function() {
+            swal("Hurry", "Thank you for your review", "success");
 
-    window.location.href = `mailto:anonyminhack5@protonmail.com?subject=${subject}&body=${body}`;
+        }, 5000);
+        form.reset();
+        
+        
+        
 
-    swal("Hurry", "Thank you for your review", "success");
-    form.reset();
-
+    }
     
 }
